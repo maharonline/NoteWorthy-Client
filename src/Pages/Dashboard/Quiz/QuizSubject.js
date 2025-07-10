9275541bc909dc9f5f80c5d698826b4589e97101
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-07-10 11:33:17 +0500

    Update QuizSubject.js

--- src/Pages/Dashboard/Quiz/QuizSubject.js
+++ src/Pages/Dashboard/Quiz/QuizSubject.js
@@ -25,8 +25,10 @@ const QuizSubjects = () => {
       <main className="flex-1 p-6">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6 gap-4">
           <h1 className="text-2xl font-bold text-center md:text-left w-full">CS Subjects</h1>
-          <div className="hidden">
+           <div className="flex flex-col sm:flex-row items-center gap-3 w-full sm:w-auto">
+    <div className="hidden">
             <ThemeToggle />
+    <div/>
             <input
               type="text"
               placeholder="Search by course title"
