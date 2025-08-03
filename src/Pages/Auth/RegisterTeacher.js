bbe29e1edfa82f43a4ee22d9cd626441cd7cd789
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-08-03 22:47:40 +0500

    Update RegisterTeacher.js

--- src/Pages/Auth/RegisterTeacher.js
+++ src/Pages/Auth/RegisterTeacher.js
@@ -1,33 +1,220 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { useAuthContext } from '../../context/AuthContext';
+
+
+// const initialstate = { userName: "", email: "", password: "", rollno: "", semester: "", };
+
+// export default function RegisterTeacher() {
+//   const navigate = useNavigate();
+//   const [state, setstate] = useState(initialstate);
+//   const [isLoading, setisLoading] = useState(false);
+//   const { themeState, ThemeToggle } = useAuthContext()
+
+//   const handleChange = (e) => setstate(s => ({ ...s, [e.target.name]: e.target.value }));
+
+//   const goToStudentRegisterPage = () => {
+//     navigate("/auth/register")
+//   }
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const { userName, email, password, employeeId } = state;
+//     if (!email || !password) return window.toastify("Please Enter Your Details", "error");
+
+//     setisLoading(true);
+//     try {
+//       const res = await axios.post(`${process.env.REACT_APP_API_URL}/teacher/registerTeacher`, { userName, email, password, employeeId, department: "Bs Computer Science" }, { withCredentials: true });
+//       window.toastify(res?.data?.msg, "success");
+
+//       navigate("/");
+//     } catch (error) {
+//       window.toastify("Registration Failed", "error");
+//       console.error(error);
+//     } finally {
+//       setisLoading(false);
+//     }
+//   };
+
+//   return (
+//     <main className="flex flex-col lg:flex-row min-h-screen bg-base-100 text-base-content font-raleway dark:text-white">
+//       {/*==== Left Side Image Section ====*/}
+//       <div className="hidden lg:block lg:w-1/2">
+//         <img
+//           src={themeState === 'dark' ? "/Assets/image/TeacherRegisterDark.png" : "/Assets/image/TeacherRegister.png"}
+//           alt="Register"
+//           className="h-screen w-full object-cover"
+//         />
+//       </div>
+
+//       {/*==== Right Side Form Section ====*/}
+//       <div className="flex justify-center flex-col items-center w-full lg:w-1/2 min-h-screen relative dashboard-bg">
+//         <div className="absolute top-0.5 left-4">
+//           <ThemeToggle />
+//         </div>
+//         <div className="absolute top-1 right-4">
+//           <button className="px-4 py-2 text-sm border border-blue-500 text-blue-600 rounded hover:bg-blue-600 hover:text-white " onClick={goToStudentRegisterPage}>
+//             Register as a Student
+//           </button>
+//         </div>
+
+
+//         <h2 className="text-3xl font-bold mt-10 ">Create New Account</h2>
+//         <p className="mb-6">Please enter your details</p>
+//         <form onSubmit={handleSubmit}
+//           className="w-full max-w-md p-6 sm:p-8 rounded-xl border  border-blue-600 shadow-md card-bg">
+//           <div className="mb-4">
+//             <label htmlFor="userName" className="text-sm block font-semibold mb-1">UserName</label>
+//             <input
+//               type="text"
+//               id="userName"
+//               name="userName"
+//               placeholder="User Name"
+//               value={state.userName}
+//               onChange={handleChange}
+//               className="auth-input"
+//             />
+//           </div>
+//           <div className="mb-4">
+//             <label htmlFor="email" className="text-sm block font-semibold mb-1">Email</label>
+//             <input
+//               type="email"
+//               id="email"
+//               name="email"
+//               placeholder="Email"
+//               value={state.email}
+//               onChange={handleChange}
+//               className="auth-input"
+//             />
+//           </div>
+//           <div className="mb-4">
+//             <label htmlFor="password" className="text-sm block font-semibold mb-1">Password</label>
+//             <input
+//               type="password"
+//               id="password"
+//               name="password"
+//               placeholder="Password"
+//               value={state.password}
+//               onChange={handleChange}
+//               className="auth-input"
+//             />
+//           </div>
+
+//           <div className="mb-4">
+//             <label htmlFor="department" className="text-sm block font-semibold mb-1">Department</label>
+//             <input
+//               type="text"
+//               id="department"
+//               name="department"
+//               placeholder="Department"
+//               defaultValue="Bs Computer Science "
+//               value={state.department}  
+//               disabled
+
+//               className="auth-input"
+//             />
+//           </div>
+//           <div className="mb-6">
+//             <label htmlFor="employeeId" className="text-sm block font-semibold mb-1">Employee ID</label>
+//             <input
+//               minLength={7}
+//               maxLength={7}
+//               id="employeeId"
+//               name="employeeId"
+//               placeholder="Enter Your Job Letter ID"
+//               value={state.employeeId}
+//               required
+//               onChange={handleChange}
+//               className="auth-input"
+//             />
+//           </div>
+
+//           <button
+//             type="submit"
+//             onClick={handleSubmit}
+//             disabled={isLoading}
+//             className="bg-blue-600 hover:bg-blue-700 rounded-md text-white w-full disabled:opacity-50 font-semibold  py-2 transition"
+//           >
+//             {isLoading ? <span className="loading loading-spinner loading-xl"></span> : 'Register'}
+//           </button>
+//           <p className="text-center text-sm mt-4">
+//             Already have an account?{' '}
+//             <Link className="text-blue-600 hover:text-blue-800" to="/auth/login">Login</Link>
+//           </p>
+
+//         </form>
+//       </div>
+//     </main>
+//   );
+// }
+
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '../../context/AuthContext';
+import { uploadToAppwrite } from '../../config/appwrite';
 
-
-const initialstate = { userName: "", email: "", password: "", rollno: "", semester: "", };
+const initialstate = {
+  userName: "",
+  email: "",
+  password: "",
+  rollno: "",
+  semester: "",
+  department: "Bs Computer Science"
+};
 
 export default function RegisterTeacher() {
   const navigate = useNavigate();
   const [state, setstate] = useState(initialstate);
   const [isLoading, setisLoading] = useState(false);
-  const { themeState, ThemeToggle } = useAuthContext()
+  const { themeState, ThemeToggle } = useAuthContext();
+  const [file, setFile] = useState(null);
 
-  const handleChange = (e) => setstate(s => ({ ...s, [e.target.name]: e.target.value }));
+  const handleChange = (e) =>
+    setstate((s) => ({ ...s, [e.target.name]: e.target.value }));
 
   const goToStudentRegisterPage = () => {
-    navigate("/auth/register")
-  }
+    navigate("/auth/register");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { userName, email, password, employeeId } = state;
-    if (!email || !password) return window.toastify("Please Enter Your Details", "error");
+    const { userName, email, password,  department } = state;
+
+    if (!userName || !email || !password ) {
+      return window.toastify("Please fill in all required fields", "error");
+    }
+
+    let jobLetterUrl = "";
+    if(!jobLetterUrl){return window.toastify("Job Letter Required","warning")}
+    if (file) {
+      try {
+       
+        jobLetterUrl = await uploadToAppwrite(file);
+        console.log("✅ Appwrite Image URL:", jobLetterUrl);
+      } catch (err) {
+        console.error("❌ Appwrite Upload Error:", err);
+        window.toastify("Failed to upload image", "error");
+        return;
+      }
+    }
 
     setisLoading(true);
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/teacher/registerTeacher`, { userName, email, password, employeeId, department: "Bs Computer Science" }, { withCredentials: true });
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/teacher/registerTeacher`,
+        {
+          userName,
+          email,
+          password,
+          
+          department,
+          jobLetterUrl,
+        },
+        { withCredentials: true }
+      );
       window.toastify(res?.data?.msg, "success");
-
       navigate("/");
     } catch (error) {
       window.toastify("Registration Failed", "error");
@@ -42,7 +229,11 @@ export default function RegisterTeacher() {
       {/*==== Left Side Image Section ====*/}
       <div className="hidden lg:block lg:w-1/2">
         <img
-          src={themeState === 'dark' ? "/Assets/image/TeacherRegisterDark.png" : "/Assets/image/TeacherRegister.png"}
+          src={
+            themeState === "dark"
+              ? "/Assets/image/TeacherRegisterDark.png"
+              : "/Assets/image/TeacherRegister.png"
+          }
           alt="Register"
           className="h-screen w-full object-cover"
         />
@@ -54,18 +245,24 @@ export default function RegisterTeacher() {
           <ThemeToggle />
         </div>
         <div className="absolute top-1 right-4">
-          <button className="px-4 py-2 text-sm border border-blue-500 text-blue-600 rounded hover:bg-blue-600 hover:text-white " onClick={goToStudentRegisterPage}>
+          <button
+            className="px-4 py-2 text-sm border border-blue-500 text-blue-600 rounded hover:bg-blue-600 hover:text-white"
+            onClick={goToStudentRegisterPage}
+          >
             Register as a Student
           </button>
         </div>
 
-
-        <h2 className="text-3xl font-bold mt-10 ">Create New Account</h2>
+        <h2 className="text-3xl font-bold mt-10">Create New Account</h2>
         <p className="mb-6">Please enter your details</p>
-        <form onSubmit={handleSubmit}
-          className="w-full max-w-md p-6 sm:p-8 rounded-xl border  border-blue-600 shadow-md card-bg">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md p-6 sm:p-8 rounded-xl border border-blue-600 shadow-md card-bg"
+        >
           <div className="mb-4">
-            <label htmlFor="userName" className="text-sm block font-semibold mb-1">UserName</label>
+            <label htmlFor="userName" className="text-sm block font-semibold mb-1">
+              UserName
+            </label>
             <input
               type="text"
               id="userName"
@@ -77,7 +274,9 @@ export default function RegisterTeacher() {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="email" className="text-sm block font-semibold mb-1">Email</label>
+            <label htmlFor="email" className="text-sm block font-semibold mb-1">
+              Email
+            </label>
             <input
               type="email"
               id="email"
@@ -89,7 +288,9 @@ export default function RegisterTeacher() {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="password" className="text-sm block font-semibold mb-1">Password</label>
+            <label htmlFor="password" className="text-sm block font-semibold mb-1">
+              Password
+            </label>
             <input
               type="password"
               id="password"
@@ -100,23 +301,23 @@ export default function RegisterTeacher() {
               className="auth-input"
             />
           </div>
-
           <div className="mb-4">
-            <label htmlFor="department" className="text-sm block font-semibold mb-1">Department</label>
+            <label htmlFor="department" className="text-sm block font-semibold mb-1">
+              Department
+            </label>
             <input
               type="text"
               id="department"
               name="department"
-              placeholder="Department"
-              defaultValue="Bs Computer Science "
-              value={state.department}  
+              value="Bs Computer Science"
               disabled
-
               className="auth-input"
             />
           </div>
-          <div className="mb-6">
-            <label htmlFor="employeeId" className="text-sm block font-semibold mb-1">Employee ID</label>
+          {/* <div className="mb-4">
+            <label htmlFor="employeeId" className="text-sm block font-semibold mb-1">
+              Employee ID
+            </label>
             <input
               minLength={7}
               maxLength={7}
@@ -124,25 +325,42 @@ export default function RegisterTeacher() {
               name="employeeId"
               placeholder="Enter Your Job Letter ID"
               value={state.employeeId}
-              required
               onChange={handleChange}
+              className="auth-input"
+              required
+            />
+          </div> */}
+
+          <div className="mb-4">
+            <label htmlFor="file" className="text-sm block font-semibold mb-1">
+              Upload Job Letter (Image)
+            </label>
+            <input
+              type="file"
+              id="file"
+              accept="image/*"
+              onChange={(e) => setFile(e.target.files[0])}
               className="auth-input"
             />
           </div>
 
           <button
             type="submit"
-            onClick={handleSubmit}
             disabled={isLoading}
-            className="bg-blue-600 hover:bg-blue-700 rounded-md text-white w-full disabled:opacity-50 font-semibold  py-2 transition"
+            className="bg-blue-600 hover:bg-blue-700 rounded-md text-white w-full disabled:opacity-50 font-semibold py-2 transition"
           >
-            {isLoading ? <span className="loading loading-spinner loading-xl"></span> : 'Register'}
+            {isLoading ? (
+              <span className="loading loading-spinner loading-xl"></span>
+            ) : (
+              "Register"
+            )}
           </button>
           <p className="text-center text-sm mt-4">
-            Already have an account?{' '}
-            <Link className="text-blue-600 hover:text-blue-800" to="/auth/login">Login</Link>
+            Already have an account?{" "}
+            <Link className="text-blue-600 hover:text-blue-800" to="/auth/login">
+              Login
+            </Link>
           </p>
-
         </form>
       </div>
     </main>
